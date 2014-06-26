1008532b7d417425122ed3ae52016d43d806075f
kgerov <kgerov@abv.bg>
2014-06-26 22:51:19 +0300

    l

--- 04. HTML5-Semantic/script/draw.js
+++ 04. HTML5-Semantic/script/draw.js
@@ -1,6 +1,15 @@
 var ctx = document.getElementById("myCanvas").getContext("2d");
-  ctx.font = "30px Arial";
-  ctx.strokeText("I love JavaScript", 10, 50);
-  ctx.moveTo(0,0);
-  ctx.lineTo(200,100);
-  ctx.stroke();
+
+ctx.font = "26px Arial";
+ctx.fillText("I love JavaScript", 5, 50);
+ctx.strokeStyle="#FF0000";
+ctx.translate(0.5, 0.5);
+ctx.beginPath();
+ctx.setLineDash([5,2]);
+ctx.rect(0,0,200,200);
+ctx.stroke();
+ctx.fillStyle = "#F1DA4E";
+ctx.fillRect(40, 70, 100, 100);
+ctx.fillStyle = "#000000"
+ctx.font = "48px Arial";
+ctx.fillText("JS", 75, 160);
